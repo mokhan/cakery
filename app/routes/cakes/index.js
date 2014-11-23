6a432a57006bfdd2abf23596c072b4a1fa76fea5
mo khan <mo@mokhan.ca>
2014-11-23 14:36:33 -0700

    load more results.

--- app/routes/cakes/index.js
+++ app/routes/cakes/index.js
@@ -3,7 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   page: 1,
   perPage: 12,
+  actions: {
+    nextPage: function(){
+      this.set('perPage', this.get('perPage') + 12);
+      var that = this;
+      this.store.find('cake', { page: 1, per_page: this.get('perPage') }).then(function(cakes){
+        that.controllerFor('cakes.index').set('content', cakes);
+      });
+    },
+  },
   model: function(params) {
     return this.store.find('cake', params);
-  }
+  },
 });
