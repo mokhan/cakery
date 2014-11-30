377d74caaa7b6d9eb8c05ffc2030baf0a421750c
mo khan <mo@mokhan.ca>
2014-11-29 23:56:01 -0700

    fix test.

--- tests/unit/helpers/formatted-date-test.js
+++ tests/unit/helpers/formatted-date-test.js
@@ -5,7 +5,7 @@ import {
 module('FormattedDateHelper');
 
 test('it renders a friendly date to display', function() {
-  var date = new Date('2014-08-02T02:30:57.182Z');
+  var date = new Date('2014-08-02T02:30:57');
   var result = formattedDate(date);
   equal(result, 'Aug 1st, 2014 at 8:30');
 });
