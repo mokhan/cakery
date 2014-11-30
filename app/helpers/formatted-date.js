c0720d6aac55204d8ca10941b8e1e2c9eaa29917
mo khan <mo@mokhan.ca>
2014-11-29 23:19:44 -0700

    add test for formatted date.

--- app/helpers/formatted-date.js
+++ app/helpers/formatted-date.js
@@ -1,8 +1,8 @@
 /* global moment:true */
 import Ember from 'ember';
 
-export function formattedDate(date, format) {
-  return moment(date).format(format);
+export function formattedDate(date) {
+  return moment(date).format('MMM Do, YYYY [at] h:mm');
 }
 
 export default Ember.Handlebars.makeBoundHelper(formattedDate);
--- tests/unit/helpers/formatted-date-test.js
+++ tests/unit/helpers/formatted-date-test.js
@@ -4,8 +4,8 @@ import {
 
 module('FormattedDateHelper');
 
-// Replace this with your real tests.
-test('it works', function() {
-  var result = formattedDate(42);
-  ok(result);
+test('it renders a friendly date to display', function() {
+  var date = new Date('2014-08-02T02:30:57.182Z');
+  var result = formattedDate(date);
+  equal(result, 'Aug 1st, 2014 at 8:30');
 });
